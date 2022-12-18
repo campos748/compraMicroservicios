0ce825412c87eeda7ee794b7433017e22b496c83
AlbertoVillasante <72270825+AlbertoVillasante@users.noreply.github.com>
2022-12-18 10:34:45 +0100

    Delete carrito directory

--- carrito/basedatos.sh
+++
@@ -1,3 +0,0 @@
-docker container run --name=tienda_cds -p 27017:27017 -d mongo
-docker cp ./carrito_inicial.csv tienda_cds:/tmp/carrito_inicial.csv
-docker exec mongo_carrito mongoimport --type csv -d bd_carrito -c carrito --headerline --drop /mongo-seed/carrito_inicial.csv
