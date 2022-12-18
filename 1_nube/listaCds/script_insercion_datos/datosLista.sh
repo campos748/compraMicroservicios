386744a5bdef4776953d36ff3d193b24b29d4da7
mCampos <72017674+campos748@users.noreply.github.com>
2022-12-18 13:47:45 +0100

    Delete 1_nube/listaCds/script_insercion_datos directory

--- 1_nube/listaCds/script_insercion_datos/datosLista.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-docker cp lista_inicial.csv tienda_cds:/tmp/lista_inicial.csv
-
-docker exec tienda_cds mongoimport --type csv -d tienda_cds -c lista_cds --headerline --drop /tmp/lista_inicial.csv
--- 1_nube/listaCds/script_insercion_datos/import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mongoimport --host tienda_cds:27017 --db tienda_cds --collection lista_cds --type csv --headerline --drop /mongo-seed/lista_inicial.csv
-mongoimport --host tienda_cds:27017 --db bd_carrito --collection carrito --type csv  --headerline --drop /mongo-seed/carrito_inicial.csv
-mongoimport --host tienda_cds:27017 --db compra_cds --collection compras --type csv  --headerline --drop /mongo-seed/compras_inicial.csv
